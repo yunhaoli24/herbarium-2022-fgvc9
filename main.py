befcd078b5314b5b589d41c23e76a1e311a21ca4
arkwq97 <354959692@qq.com>
2022-09-30 20:47:09 +0800

    添加数据集预览

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
 
     target_transform = transforms.Compose([
         transforms.Resize(size=(224, 224)),
+        transforms.Normalize(mean=0.5, std=0.5),
         transforms.ToTensor(),
     ])
 
---
+++ src/data_view.py
@@ -0,0 +1,4 @@
+import json
+with open('/dataset/cv/cls/herbarium-2022-fgvc9/train_metadata.json', "r") as f:
+    traindata = json.load(f)
+print(1)
